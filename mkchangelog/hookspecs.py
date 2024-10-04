8b9217a79954c5badfdd33618cfaa88de1e7d255
Marek Wywiał <onjinx@gmail.com>
2024-10-04 22:27:36 +0200

    style: changes for new ruff

--- mkchangelog/hookspecs.py
+++ mkchangelog/hookspecs.py
@@ -10,10 +10,10 @@ hookspec = pluggy.HookspecMarker("mkchangelog")
 
 
 @hookspec
-def provide_template_filters(settings: Settings) -> Dict[str, Callable[[Any], str]]:  # noqa: ARG001
+def provide_template_filters(settings: Settings) -> Dict[str, Callable[[Any], str]]:
     """Return dictionary of Jinja2 filters to be used in template."""
 
 
 @hookspec
-def provide_changelog_loglines_filter(settings: Settings) -> Callable[[List[LogLine]], List[LogLine]]:  # noqa: ARG001
+def provide_changelog_loglines_filter(settings: Settings) -> Callable[[List[LogLine]], List[LogLine]]:
     """Filter changelog loglines"""
